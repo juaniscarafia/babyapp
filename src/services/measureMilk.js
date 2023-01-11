1e07f5e643654d877443cf2ef7529e9881162662
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2023-01-11 16:24:38 -0300

    update urls api

--- src/services/measureMilk.js
+++ src/services/measureMilk.js
@@ -6,7 +6,8 @@ const measureMilks = {};
 measureMilks.search = async function() {
   // return await axios.get('http://localhost:3004/api/measuremilks')
   // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks')
-  return await axios.get('https://apibabyapp.onrender.com/api/measuremilks')
+  // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks')
+  return await axios.get('https://f9a2-190-244-83-15.sa.ngrok.io/api/measuremilks')
     .then(res => res)
     .catch(error => error);
 };
@@ -14,7 +15,8 @@ measureMilks.search = async function() {
 measureMilks.listMilks = async function() {
   // return await axios.get('http://localhost:3004/api/measuremilks/milks')
   // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/milks')
-  return await axios.get('https://apibabyapp.onrender.com/api/measuremilks/milks')
+  // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks/milks')
+  return await axios.get('https://f9a2-190-244-83-15.sa.ngrok.io/api/measuremilks/milks')
     .then(res => res)
     .catch(error => error);
 };
@@ -22,7 +24,8 @@ measureMilks.listMilks = async function() {
 measureMilks.insert = async function(data) {
   // return await axios.post('http://localhost:3004/api/measuremilks/insertmeasure',data)
   // return await axios.post('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/insertmeasure',data)
-  return await axios.post('https://apibabyapp.onrender.com/api/measuremilks/insertmeasure',data)
+  // return await axios.post('https://apibabyapp.onrender.com/api/measuremilks/insertmeasure',data)
+  return await axios.post('https://f9a2-190-244-83-15.sa.ngrok.io/api/measuremilks/insertmeasure',data)
     .then(res => res)
     .catch(error => error);
 };
