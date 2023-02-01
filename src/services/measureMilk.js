c7b25ea745ad0356f1f37850560438efd81b3a80
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2023-02-01 20:10:44 -0300

    change url api

--- src/services/measureMilk.js
+++ src/services/measureMilk.js
@@ -7,7 +7,7 @@ measureMilks.search = async function() {
   // return await axios.get('http://localhost:3004/api/measuremilks')
   // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks')
   // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks')
-  return await axios.get('https://ef6b-190-244-83-15.sa.ngrok.io/api/measuremilks')
+  return await axios.get('https://80c6-190-244-83-15.sa.ngrok.io/api/measuremilks')
     .then(res => res)
     .catch(error => error);
 };
@@ -16,7 +16,7 @@ measureMilks.listMilks = async function() {
   // return await axios.get('http://localhost:3004/api/measuremilks/milks')
   // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/milks')
   // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks/milks')
-  return await axios.get('https://ef6b-190-244-83-15.sa.ngrok.io/api/measuremilks/milks')
+  return await axios.get('https://80c6-190-244-83-15.sa.ngrok.io/api/measuremilks/milks')
     .then(res => res)
     .catch(error => error);
 };
@@ -25,7 +25,7 @@ measureMilks.insert = async function(data) {
   // return await axios.post('http://localhost:3004/api/measuremilks/insertmeasure',data)
   // return await axios.post('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/insertmeasure',data)
   // return await axios.post('https://apibabyapp.onrender.com/api/measuremilks/insertmeasure',data)
-  return await axios.post('https://ef6b-190-244-83-15.sa.ngrok.io/api/measuremilks/insertmeasure',data)
+  return await axios.post('https://80c6-190-244-83-15.sa.ngrok.io/api/measuremilks/insertmeasure',data)
     .then(res => res)
     .catch(error => error);
 };
