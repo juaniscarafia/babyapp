8c8ce72ef8dca3f0191f06c25cca22c1971887d2
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2023-03-11 07:38:36 -0300

    fix url api

--- src/services/measureMilk.js
+++ src/services/measureMilk.js
@@ -2,30 +2,25 @@
 import axios from "axios";
 
 const measureMilks = {};
+let apiUrl = 'https://420f-190-244-83-15.sa.ngrok.io'
+// let apiUrl = 'http://localhost:3004'
+// let apiUrl = 'https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com'
+// let apiUrl = 'https://apibabyapp.onrender.com'
 
 measureMilks.search = async function() {
-  // return await axios.get('http://localhost:3004/api/measuremilks')
-  // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks')
-  // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks')
-  return await axios.get('https://8d8d-190-244-83-15.sa.ngrok.io/api/measuremilks')
+  return await axios.get(`${apiUrl}/api/measuremilks`)
     .then(res => res)
     .catch(error => error);
 };
 
 measureMilks.listMilks = async function() {
-  // return await axios.get('http://localhost:3004/api/measuremilks/milks')
-  // return await axios.get('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/milks')
-  // return await axios.get('https://apibabyapp.onrender.com/api/measuremilks/milks')
-  return await axios.get('https://8d8d-190-244-83-15.sa.ngrok.io/api/measuremilks/milks')
+  return await axios.get(`${apiUrl}/api/measuremilks/milks`)
     .then(res => res)
     .catch(error => error);
 };
 
 measureMilks.insert = async function(data) {
-  // return await axios.post('http://localhost:3004/api/measuremilks/insertmeasure',data)
-  // return await axios.post('https://port-3004-lucecitapp-juaniscarafia231893.codeanyapp.com/api/measuremilks/insertmeasure',data)
-  // return await axios.post('https://apibabyapp.onrender.com/api/measuremilks/insertmeasure',data)
-  return await axios.post('https://8d8d-190-244-83-15.sa.ngrok.io/api/measuremilks/insertmeasure',data)
+  return await axios.post(`${apiUrl}/api/measuremilks/insertmeasure`,data)
     .then(res => res)
     .catch(error => error);
 };
